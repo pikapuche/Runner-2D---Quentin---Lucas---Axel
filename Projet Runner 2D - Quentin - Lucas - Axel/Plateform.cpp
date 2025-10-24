96280422517cc7e3f860a982b10c5783b7417578
0ri0546 <orio60@outlook.fr>
2025-10-24 16:18:28 +0200

    fix

--- Projet Runner 2D - Quentin - Lucas - Axel/Plateform.cpp
+++ Projet Runner 2D - Quentin - Lucas - Axel/Plateform.cpp
@@ -6,7 +6,7 @@ Plateform::Plateform(float _velocity, int _line) : line(_line), velocity(_veloci
 	if (!shapeTexture.loadFromFile("Assets/tiles_map/RunnerTileSet.png")) {
 		std::cerr << "Erreur chargement texture" << std::endl;
 	}
-	shape.setTextureRect(sf::IntRect({ 320, 0 }, { 96, 32 })); // 1 tile = 32px //// 3 tiles = 96px
+	shape.setTextureRect(sf::IntRect({ 320, 0 }, { 96, 16 })); // 1 tile = 32px //// 3 tiles = 96px
 	shape.setTexture(&shapeTexture);
 	shape.setFillColor(sf::Color::Green);
 }
