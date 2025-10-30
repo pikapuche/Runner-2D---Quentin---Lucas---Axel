a62ff34fae1bbfb8fa1b66154413ca654ccebd78
0ri0546 <orio60@outlook.fr>
2025-10-30 21:54:22 +0100

    clean Pause

--- Projet Runner 2D - Quentin - Lucas - Axel/Pause.cpp
+++ Projet Runner 2D - Quentin - Lucas - Axel/Pause.cpp
@@ -3,57 +3,48 @@ Pause::Pause() : text(textFont) {
 	initMenu();
 }
 
-Pause::~Pause()
-{
-}
-void Pause::initMenu() {
+Pause::~Pause() {}
 
-	//setup background
-	background.setSize({ 1920,1080 });
+void Pause::initMenu() {
+	if (!textFont.openFromFile("Assets/Fonts/Minecraft.ttf"))
+		std::cerr << "error : to open minecraft font" << std::endl;
+
+	background.setSize({ static_cast<float>(STGS::WIDTH), static_cast<float>(STGS::HEIGHT) });
 	background.setPosition({ 0,0 });
 	background.setFillColor(sf::Color(0, 0, 0, 100));
 
-	//setup backButton
 	backButton.buttonState = Button::BACK;
-	backButton.buttonBack.setPosition({ static_cast<float>(STGS::WIDTH * 0.05),800 });
 	backButton.buttonBack.setFillColor(sf::Color::White);
-	backButton.buttonBack.setSize({ 300,60 });
-
-	backButton.buttonFont.openFromFile("Assets/Fonts/Minecraft.ttf");
+	backButton.buttonBack.setSize({ static_cast<float>(STGS::WIDTH * 0.4), static_cast<float>(STGS::HEIGHT * 0.06) });
+	backButton.buttonBack.setPosition({ static_cast<float>(STGS::WIDTH * 0.05), static_cast<float>(STGS::HEIGHT * 0.8) });
 
 	backButton.buttonText.setCharacterSize(30);
-	backButton.buttonText.setFont(backButton.buttonFont);
+	backButton.buttonText.setFont(textFont);
 	backButton.buttonText.setString("BACK");
 	backButton.buttonText.setPosition({ backButton.buttonBack.getPosition().x + backButton.buttonBack.getGlobalBounds().size.x / 2 - backButton.buttonText.getGlobalBounds().size.x / 2,backButton.buttonBack.getPosition().y + backButton.buttonText.getGlobalBounds().size.y / 2 });
 	backButton.buttonText.setFillColor(sf::Color::Black);
 
-	//setup resumeButton
 	resumeButton.buttonState = Button::RESTART;
-	resumeButton.buttonBack.setPosition({ static_cast<float>(STGS::WIDTH * 0.05),700 });
 	resumeButton.buttonBack.setFillColor(sf::Color::White);
-	resumeButton.buttonBack.setSize({ 300,60 });
+	resumeButton.buttonBack.setSize({ static_cast<float>(STGS::WIDTH * 0.4), static_cast<float>(STGS::HEIGHT * 0.06) });
+	resumeButton.buttonBack.setPosition({ static_cast<float>(STGS::WIDTH * 0.05), static_cast<float>(STGS::HEIGHT * 0.7) });
 
-	resumeButton.buttonFont.openFromFile("Assets/Fonts/Minecraft.ttf");
-	resumeButton.buttonText.setFont(resumeButton.buttonFont);
+	resumeButton.buttonText.setFont(textFont);
 	resumeButton.buttonText.setCharacterSize(30);
 	resumeButton.buttonText.setString("RESUME");
 	resumeButton.buttonText.setPosition({ resumeButton.buttonBack.getPosition().x + resumeButton.buttonBack.getGlobalBounds().size.x/2 - resumeButton.buttonText.getGlobalBounds().size.x/2,resumeButton.buttonBack.getPosition().y + resumeButton.buttonText.getGlobalBounds().size.y / 2});
 	resumeButton.buttonText.setFillColor(sf::Color::Black);
 
-	//setup text
-	textFont.openFromFile("Assets/Fonts/Minecraft.ttf");
 	text.setFont(textFont);
 	text.setString("PAUSE");
 	text.setCharacterSize(100);
 	text.setFillColor(sf::Color::Green);
 	text.setOutlineColor(sf::Color(150, 150, 150));
 	text.setOutlineThickness(2);
-	text.setPosition({ static_cast<float>(STGS::WIDTH / 2) - text.getGlobalBounds().size.x / 2 ,200 });
-
+	text.setPosition({ static_cast<float>(STGS::WIDTH / 2) - text.getGlobalBounds().size.x / 2, static_cast<float>(STGS::HEIGHT * 0.2) });
 }
 
 void Pause::drawMenu(sf::RenderWindow& window) {
-
 	window.draw(background);
 	backButton.drawButton(window);
 	resumeButton.drawButton(window);
@@ -61,5 +52,5 @@ void Pause::drawMenu(sf::RenderWindow& window) {
 }
 
 void Pause::createStartButton() {
-
+	//PASS
 }
